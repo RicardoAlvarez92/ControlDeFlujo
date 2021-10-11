ee71fd4353dd2acbf43b08fb404c8e6234489690
Ricardo Daniel <Ricperez0101@gmail.com>
2021-10-11 17:26:03 -0500

    CONTROL DE FLUJO

---
+++ src/com/generation/Main.java
@@ -0,0 +1,110 @@
+package com.generation;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Estructura de control if
+
+       /* Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa tu edad");
+        int edad = sc.nextInt();
+
+        System.out.println("¿Cual es tu nombre?");
+        sc.nextLine();
+        String nombre = sc.nextLine();
+
+        if(edad > 18) {
+            System.out.println("felicidades " + nombre + " puedes votar");
+        }else{
+            System.out.println("Hola "+nombre+" .aun no puedes votar");
+        }
+        sc.close();*/
+        /*
+        System.out.println("¿Cual es tu nombre?");
+        sc.nextLine();
+        */
+
+        /* Solicitar al usuario 3 angulos de un triangulo y determinar si el triangulo es valido */
+
+//        Scanner angs = new Scanner (System.in);
+//
+//        System.out.println("ingresa el primer angulo");
+//        int ang1 = angs.nextInt();
+//
+//        System.out.println("ingresa el segundo angulo");
+//        int ang2 = angs.nextInt();
+//
+//        System.out.println("ingresa el tercer angulo");
+//        int ang3 = angs.nextInt();
+//
+//
+//        if(ang1 + ang2 + ang3 == 180 ) {
+//            System.out.println("Es un triangulo");
+//        }else{
+//           System.out.println("No es un triangulo");
+//        }
+//        angs.close();
+
+//        Scanner y = new Scanner (System.in);
+//        System.out.println("Ingresa el año");
+//        int year = y.nextInt();
+//
+//        if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))) {
+//            System.out.println("Este año es bisiesto");
+//        }else {
+//            System.out.println("Este año no es bisiesto");
+//        }
+//
+//        y.close();
+
+        // control de flujo switch
+//        switch(condicion){
+//            case 1:
+//                codigo
+//            break;
+//
+//            case 2:
+//                codigo
+//            break;
+//
+//            default:
+//            break;
+//        }
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Ingresa un numero");
+//        int numero1 = sc.nextInt();
+//        System.out.println("Ingresa otro numero");
+//        int numero2 = sc.nextInt();
+//        System.out.println("Ingresa la operacion deseada: \n1) Suma \n2) Resta " +
+//                "\n3) Multiplicación \n4) Division");
+//        int operacion = sc.nextInt();
+//        sc.nextLine();
+//        float resultado = 0;
+//
+//        switch (operacion) {
+//            case 1:
+//                resultado = numero1 +numero2;
+//                break;
+//            case 2:
+//                resultado = numero1 - numero2;
+//                break;
+//            case 3:
+//                resultado = numero1 * numero2;
+//                break;
+//            case 4:
+//                resultado = numero1 / numero2;
+//                break;
+//            default: System.out.println("La operacion que seleccionaste es invalida");
+//            break;
+//        }
+//        sc.close();
+//        System.out.println(resultado);
+
+
+    }
+}
+
+
+
